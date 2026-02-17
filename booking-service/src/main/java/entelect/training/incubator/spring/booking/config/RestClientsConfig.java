51c865a7b3c0f9ef81713b782c46dce0d6ee32dd
AloneJoubert-code <alone.joubert@entelect.co.za>
2026-02-17 08:58:13 +0200

    Add loyalty service configuration
    Add loyalty-service base URL to application properties and configure
    Jaxb2Marshaller and WebServiceTemplate beans for SOAP communication
    with the loyalty rewards endpoint.

--- booking-service/src/main/java/entelect/training/incubator/spring/booking/config/RestClientsConfig.java
+++ booking-service/src/main/java/entelect/training/incubator/spring/booking/config/RestClientsConfig.java
@@ -3,7 +3,9 @@ package entelect.training.incubator.spring.booking.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.web.client.RestClient;
+import org.springframework.ws.client.core.WebServiceTemplate;
 
 @Configuration
 public class RestClientsConfig {
@@ -28,5 +30,23 @@ public class RestClientsConfig {
                     return execution.execute(request, body);
                 })
                 .build();
+    }
+
+    @Bean
+    public Jaxb2Marshaller loyaltyMarshaller() {
+        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+        marshaller.setContextPath("entelect.training.incubator.spring.booking.ws.model");
+        return marshaller;
+    }
+
+    @Bean
+    public WebServiceTemplate loyaltyWebServiceTemplate(
+            @Value("${services.loyalty.base-url}") String loyaltyBaseUrl,
+            Jaxb2Marshaller loyaltyMarshaller) {
+        WebServiceTemplate template = new WebServiceTemplate();
+        template.setDefaultUri(loyaltyBaseUrl);
+        template.setMarshaller(loyaltyMarshaller);
+        template.setUnmarshaller(loyaltyMarshaller);
+        return template;
     }
 }
