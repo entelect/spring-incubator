90dd8574c69f0907d256f0063038067c3c4d1014
AloneJoubert-code <alone.joubert@entelect.co.za>
2026-02-17 08:56:38 +0200

    Add booking-service test scaffold
    Add initial Spring Boot test class for the booking-service module
    with explicit application class reference for package mismatch.

---
+++ booking-service/src/test/java/entelect/training/incubator/bookingservice/BookingServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package entelect.training.incubator.bookingservice;
+
+import entelect.training.incubator.spring.booking.BookingServiceApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = BookingServiceApplication.class)
+class BookingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
